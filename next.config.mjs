1523c91645789af4e053d62b789e164253cbe06a
milunusu <milanesaruggiero@gmail.com>
2024-03-18 08:22:15 -0400

    vercelerror

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 import path from 'path';
 
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
 const nextConfig = {
   webpack: (config) => {
     config.resolve.alias['@'] = path.resolve(__dirname, 'utils');
